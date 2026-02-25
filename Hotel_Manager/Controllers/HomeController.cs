1a78847c9122f45299b57bd7778d3c3428ded2d6
arsenovb <b_arsenov@abv.bg>
2026-02-25 23:11:05 +0200

    Add "Today" board to index page

--- Hotel_Manager/Controllers/HomeController.cs
+++ Hotel_Manager/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
+using Hotel_Manager.Data;
 using Hotel_Manager.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Hotel_Manager.Controllers
@@ -8,13 +10,29 @@ namespace Hotel_Manager.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            if (User.Identity?.IsAuthenticated == true &&
+               (User.IsInRole("Receptionist") || User.IsInRole("Admin")))
+            {
+                ViewBag.UpcomingReservations = await _context.Reservations
+                    .CountAsync(r => r.Status == "Upcoming");
+
+                ViewBag.InProgressReservations = await _context.Reservations
+                    .CountAsync(r => r.Status == "In progress");
+
+                ViewBag.FreeRooms = await _context.Rooms
+                    .CountAsync(r => r.IsAvailable);
+            }
+
             return View();
         }
 
